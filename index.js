e661b2da3b35af30fe0ba44cae9825375133e58a
Ray <rjmaestas1991@gmail.com>
2022-11-01 10:44:45 -0600

    commit tree 2a80455e2889f99c43dd937d933b7a50eb69fe35

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+ const cats = ['Milo','Otis','Garfield']
+
+ function destructivelyAppendCat(){
+    return cats.push('Ralph')
+ }
+ function destructivelyPrependCat(){
+    return cats.unshift('Bob')
+ }
+
+ function destructivelyRemoveLastCat(){
+    return cats.pop('Garfield')
+ }
+
+ function destructivelyRemoveFirstCat(){
+    return cats.shift('Milo')
+ }
+
+ function appendCat(){
+    return ['Milo','Otis','Garfield', 'Broom']
+ }
+
+ function prependCat(){
+    return ['Arnold','Milo','Otis','Garfield',]
+ }
+
+ function removeLastCat(){
+    return cats.slice(0, cats.length - 1)
+ }
+
+ function removeFirstCat(){
+    return cats.slice(1)
+ }
